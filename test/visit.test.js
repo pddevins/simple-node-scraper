8f9c7385b8368b21f7f0a2678600ad0bfa947d0b
Phil Palmieri <ppalmieri@partnercomm.net>
2018-07-12 09:01:52 -0400

    Remove legacy info

--- test/visit.test.js
+++ test/visit.test.js
@@ -1,5 +1,4 @@
 const visit = require('../visit')
-const helpers = require('../helpers');
 const nock = require('nock');
 const fs = require('fs-then-native');
 const oddball = fs.readFileSync('./test/data/index.html', 'utf8');
@@ -52,22 +51,4 @@ describe('visit', () => {
 
         done();
     });
-
-    // it('should work for a single page', function () {
-    //     const { input, expected } = helpers.singlePage();
-    //     const sitemap = visit(input);
-    //     sitemap.forEach(page => expect(expected).to.contain(page));
-    // });
-
-    // it('should work with multiple pages', function () {
-    //     const { input, expected } = helpers.multiplePages();
-    //     const sitemap = visit(input);
-    //     sitemap.forEach(page => expect(expected).to.contain(page));
-    // });
-
-    // it('should work with pages that have nested links', function () {
-    //     const { input, expected } = helpers.pagesWithLinks();
-    //     const sitemap = visit(input);
-    //     sitemap.forEach(page => expect(expected).to.contain(page));
-    // });
 });
